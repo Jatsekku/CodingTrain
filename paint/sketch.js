e07daa6d33517a731778dd4d0461675ec36ece78
Jatsekku <jatsekku2@gmail.com>
2023-01-20 00:41:24 +0100

    paint: mouseX/Y variables

---
+++ paint/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+	createCanvas(400, 400);
+}
+
+function draw() {
+	noStroke();
+	fill(255, 150);
+	circle(mouseX, mouseY, 24)
+}
+
+function mousePressed() {
+	background(0);
+}

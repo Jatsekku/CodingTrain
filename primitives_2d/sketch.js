8ec77966db00d0300f2288ac7ac0e0f48104f177
Jatsekku <jatsekku2@gmail.com>
2023-01-20 00:14:53 +0100

    primitives_2d: Playing with colors and outlines

--- primitives_2d/sketch.js
+++ primitives_2d/sketch.js
@@ -6,13 +6,19 @@ function draw() {
 	background(255, 0, 255);
 	
 	rectMode(RADIUS);
+	fill(0, 0, 255);
+	stroke(0, 255, 0);
 	rect(200, 200, 125, 125);
 	
+	stroke(0);
+
+	fill(255, 0, 0);
 	line(10, 10, 389, 10);
 	line(389, 10, 389, 389);
 	line(389, 389, 10, 389);
 	line(10, 389, 10, 10);
 
+	fill(0, 255, 0);
 	ellipse(50, 50, 15, 40);
 	ellipse(350, 50, 15, 40);
 	ellipse(50, 350, 15, 40);
